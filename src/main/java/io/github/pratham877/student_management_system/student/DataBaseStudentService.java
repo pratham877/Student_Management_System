e0175bcdaa6278dc20c5e7109f75680aeefe770e
Aman-wq-cmk <amansoni2868@gmail.com>
2025-04-13 21:25:55 +0530

    started a new package named academy

--- src/main/java/io/github/pratham877/student_management_system/student/DataBaseStudentService.java
+++ src/main/java/io/github/pratham877/student_management_system/student/DataBaseStudentService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class DataBaseStudentService {
+public class DataBaseStudentService implements StudentService {
 
     private  StudentRepository studentRepository;
 
@@ -19,5 +19,61 @@ public class DataBaseStudentService {
 
     public List<Student> addStudents(Student students) {
 
+        return List.of();
+    }
+
+    @Override
+    public List<Student> addStudent(Student student) {
+        return List.of();
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return List.of();
+    }
+
+    @Override
+    public Student getStudentById(long id) {
+        return null;
+    }
+
+    @Override
+    public Student getStudentByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudentName(long id, String newFullName) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudentEmail(long id, String newEmail) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudentPassword(long id, String newPassword) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudentPhone(long id, String newPhone) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudentAddress(long id, String newAddress) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudentAge(long id, int newAge) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteStudent(long id) {
+        return false;
     }
 }
